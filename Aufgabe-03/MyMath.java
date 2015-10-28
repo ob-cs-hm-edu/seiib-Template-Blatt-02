c3288f2b7d821e7a0ea64289f8e0a33ab4c78e1e
Oliver Braun <ob@obraun.net>
2015-10-28 09:03:08 +0100

    Add scrambled MyMath file.

---
+++ Aufgabe-03/MyMath.java
@@ -0,0 +1,8 @@
+class MyMath { public static main(String[] args) { final input
+= Double.parseDouble(args[0]); System.out.printf("sin(%f)
+= %f\n", input, Math.sinus(input)); System.out.printf("cos(%f)
+= %f\n", input Math.cos(input)); System.out.printf("tangens(%f)
+cosinus(%f) = %f\n", input, Math.tan(input) Math.cos(input));
+System.out.printf("e hoch %f = %f\n, input, Math.exp(input));
+System.out.print("log(e hoch %f) = %f\n", input,
+Math.log(Math.exp(input))); } }
